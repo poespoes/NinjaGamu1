99708254e7214b51440553aa5b663e96354f19fa
Unknown <poe_s@hotmail.com>
2020-12-11 16:36:38 +0700

    Test1 scene update + BlendModes

---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointController : MonoBehaviour
+{
+    public KeyCode KeyCode1;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode1)) {
+            anim.SetTrigger("toNextPoint");
+
+        }
+        
+    }
+}
